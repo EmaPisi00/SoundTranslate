9db2d389781c77127a6c4c52bd9f6b626c9d2676
EmaPisi00 <emanuelepisaturo@hotmail.it>
2025-02-20 15:12:08 +0100

    avanzamento

--- src/main/java/it/projects/translate/ui/controller/ConvertAudioController.java
+++ src/main/java/it/projects/translate/ui/controller/ConvertAudioController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
@@ -20,33 +21,83 @@ import java.util.List;
 
 public class ConvertAudioController {
 
-    private File selectedFolder;
-
-    // Riferimento alla Label e al ProgressIndicator per mostrare il processo
     @FXML
-    private Label selectedFolderLabel;
+    private Label selectedFolderInputLabel;
+
+    @FXML
+    private Label selectedFolderOutputLabel;
 
     @FXML
     private ProgressIndicator progressIndicator;
 
     @FXML
+    private TextField folderPathInputField; // TextField dove l'utente può incollare il percorso
+
+    @FXML
+    private TextField folderPathOutputField;
+
+    private File selectedInputFolder = null;
+    private File selectedOutputFolder = null;
+
+    @FXML
+    public void initialize() {
+        setupFolderPathListener(folderPathInputField, selectedFolderInputLabel, true);
+        setupFolderPathListener(folderPathOutputField, selectedFolderOutputLabel, false);
+    }
+
+    @FXML
     public void onChooseFolder() {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         directoryChooser.setTitle("Select Folder");
+
+        // Se il campo di testo ha un percorso valido, usalo come cartella iniziale
+        File initialDirectory = new File(folderPathInputField.getText());
+        if (initialDirectory.exists() && initialDirectory.isDirectory()) {
+            directoryChooser.setInitialDirectory(initialDirectory);
+        } else {
+            directoryChooser.setInitialDirectory(selectedInputFolder); // Usa la cartella predefinita
+        }
+
         File folder = directoryChooser.showDialog(new Stage());
 
         if (folder != null) {
-            selectedFolder = folder;
+            selectedInputFolder = folder;
             System.out.println("Selected folder: " + folder.getAbsolutePath());
 
-            // Aggiorna la label con il nome della cartella selezionata
-            selectedFolderLabel.setText("Cartella selezionata: " + folder.getName());
+            // Aggiorna il campo di testo e la label con il percorso selezionato
+            folderPathInputField.setText(folder.getAbsolutePath());
+            selectedFolderInputLabel.setText("Cartella selezionata: " + folder.getName());
+        }
+    }
+
+    @FXML
+    public void onChooseFolderExcel() {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Select Folder");
+
+        // Se il campo di testo ha un percorso valido, usalo come cartella iniziale
+        File initialDirectory = new File(folderPathOutputField.getText());
+        if (initialDirectory.exists() && initialDirectory.isDirectory()) {
+            directoryChooser.setInitialDirectory(initialDirectory);
+        } else {
+            directoryChooser.setInitialDirectory(selectedOutputFolder); // Usa la cartella predefinita
+        }
+
+        File folder = directoryChooser.showDialog(new Stage());
+
+        if (folder != null) {
+            selectedOutputFolder = folder;
+            System.out.println("Selected folder: " + folder.getAbsolutePath());
+
+            // Aggiorna il campo di testo e la label con il percorso selezionato
+            folderPathOutputField.setText(folder.getAbsolutePath());
+            selectedFolderOutputLabel.setText("Cartella selezionata: " + folder.getName());
         }
     }
 
     @FXML
     public void onGenerateExcel() throws IOException {
-        if (selectedFolder == null) {
+        if (selectedInputFolder == null || selectedOutputFolder == null) {
             System.out.println("No folder selected. Please choose a folder first.");
             return;
         }
@@ -66,10 +117,30 @@ public class ConvertAudioController {
     }
 
     private File chooseOutputFile() {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Save Excel File");
-        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Excel Files", "*.xlsx"));
-        return fileChooser.showSaveDialog(new Stage());
+        String folderPath = folderPathOutputField.getText();
+        File outputDirectory = new File(folderPath);
+
+        // Verifica se la cartella esiste, altrimenti usa la cartella predefinita
+        if (!outputDirectory.exists() || !outputDirectory.isDirectory()) {
+            outputDirectory = selectedOutputFolder;
+        }
+
+        // Ottieni il nome del file dall'input dell'utente
+        String filePath = folderPathOutputField.getText().trim(); // Ottieni il percorso dal TextField
+        File file = new File(filePath);
+
+        String fileName = file.getName(); // Estrai solo il nome del file
+
+        // Se il percorso è vuoto o non contiene un file valido, assegna un nome predefinito
+        if (filePath.isEmpty() || !fileName.contains(".")) {
+            fileName = "file_output.xlsx";
+        }
+
+        System.out.println("Nome del file: " + fileName);
+
+
+        // Crea il file nella cartella selezionata
+        return new File(outputDirectory, fileName);
     }
 
     private void startGenerationTask(File file) {
@@ -106,8 +177,8 @@ public class ConvertAudioController {
             AudioRecognitionService audioRecognitionService = new AudioRecognitionService(modelFile.getAbsolutePath());
             TranslationService translationService = new TranslationService();
 
-            List<String> fileNames = audioRecognitionService.getFileNames(selectedFolder.getAbsolutePath());
-            List<String> recognizedTexts = audioRecognitionService.getRecognizedTexts(fileNames, selectedFolder.getAbsolutePath());
+            List<String> fileNames = audioRecognitionService.getFileNames(selectedInputFolder.getAbsolutePath());
+            List<String> recognizedTexts = audioRecognitionService.getRecognizedTexts(fileNames, selectedInputFolder.getAbsolutePath());
             List<String> translations = translationService.getTranslations(recognizedTexts);
 
             // Scrivi i dati nel file Excel
@@ -158,4 +229,26 @@ public class ConvertAudioController {
 
         alert.showAndWait();
     }
+
+    // Metodo per centralizzare la logica del listener e aggiornare le label di input e output
+    private void setupFolderPathListener(TextField textField, Label label, boolean isFolderPath) {
+        textField.textProperty().addListener((observable, oldValue, newValue) -> {
+            File file = new File(newValue);
+
+            if (isFolderPath) { // Se deve essere una cartella valida
+                if (file.exists() && file.isDirectory()) {
+                    label.setText("Cartella selezionata: " + file.getName());
+                } else {
+                    label.setText("Percorso non valido");
+                }
+            } else { // Se deve essere un file (anche inesistente)
+                if (!newValue.trim().isEmpty()) {
+                    label.setText("File selezionato: " + file.getName());
+                } else {
+                    label.setText("Percorso non valido");
+                }
+            }
+        });
+    }
+
 }
